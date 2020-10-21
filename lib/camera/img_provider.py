360c9665e0c626b56a7e31aa29b9589f6b29b16b
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-10-21 14:52:35 -0400

    Added basic python image capture

---
+++ lib/camera/img_provider.py
@@ -0,0 +1,26 @@
+from picamera import PiCamera
+from time import sleep
+import numpy as np
+
+IMG_WIDTH = 2592
+IMG_HEIGHT = 1944
+DEFAULT_FRAMERATE = 24
+
+
+class ImageProvider:
+    def __init__(
+        self,
+        sleep_timer=5,
+        resolution=(IMG_HEIGHT, IMG_WIDTH),
+    ):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = DEFAULT_FRAMERATE
+        self.sleep_timer = sleep_timer
+        sleep(sleep_timer)
+
+    def getImg(self):
+        self.camera.start_preview()
+        sleep(self.sleep_timer)
+        self.camera.capture("captured_img/img.jpg")
+        self.camera.stop_preview()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
 from lib.speech.text_to_speech import TextToSpeech
+from lib.camera.img_provider import ImageProvider
 
 
 def main():
-    tts = TextToSpeech()
-    tts.say("Hello World")
+    img_provider = ImageProvider()
+    img_provider.getImg()
 
 
 if __name__ == "__main__":
