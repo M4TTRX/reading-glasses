b37d218365987140ccbcfc81fade40e5a4db7317
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-10-21 22:06:19 -0400

    set up image capture saving in the image provider

--- lib/camera/img_provider.py
+++ lib/camera/img_provider.py
@@ -1,6 +1,8 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import sleep
+import os
+from PIL import Image
 
 IMG_WIDTH = 2592
 IMG_HEIGHT = 1944
@@ -10,23 +12,35 @@ DEFAULT_FRAMERATE = 24
 class ImageProvider:
     def __init__(
         self,
-        path,
         sleep_timer=2,
         resolution=(IMG_HEIGHT, IMG_WIDTH),
     ):
         self.camera = PiCamera()
-        self.sleep_timer = sleep_timer
-        self.path = path
         sleep(sleep_timer)
 
-    def getImg(self):
+    def getImg(self, sleep_timer=1, verbose=False, save_image=False):
         # self.camera.capture(self.path)
-
+        if verbose:
+            print("Initiating camera capture...")
         # grab a reference to the raw cam capture
         rawCapture = PiRGBArray(self.camera)
 
         # allow the camera to warmup
         sleep(self.sleep_timer)
 
+        # capture the image
         self.camera.capture(rawCapture, format="rgb")
-        return rawCapture.array
+        if verbose:
+            print("Image capture completed")
+
+        img_arr = rawCapture.array
+        # save the image if needed
+        if save_image:
+            from datetime import datetime
+
+            img_name = datetime.now().strftime("%d/%m/%Y_%H:%M:%S") + ".png"
+
+            img = Image.fromarray(img_arr)
+            img.save(img_name)
+
+        return img_arr
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from lib.speech.text_to_speech import TextToSpeech
 from lib.camera.img_provider import ImageProvider
-import os
-from PIL import Image
 
 
 def main():
     path = os.path.abspath(os.getcwd())
-    print(path)
     img_provider = ImageProvider(path=f"{path}/captured/img.jpg")
     img_arr = img_provider.getImg()
     im = Image.fromarray(img_arr)
