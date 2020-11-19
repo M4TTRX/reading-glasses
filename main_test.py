9dff123b5780261bd5c896ecb3500c6419bb7861
Matthieu Roux <matthieurouxleoncini@gmail.com>
2020-11-19 00:03:58 -0500

    Say the tesxt now

--- main_test.py
+++ main_test.py
@@ -5,16 +5,19 @@ from lib.speech.text_to_speech import TextToSpeech
 
 def test_pc():
     ocr = OCR(tesseract_path="C:\\Program Files\\Tesseract-OCR\\tesseract.exe")
+    tts = TextToSpeech()
+
     img = cv2.imread("lib/ocr/test_images/PXL_20201117_200200158.jpg")
     text = ocr.get_text(img)
     print(text)
+    tts.say(text)
+    return
 
 
 def test():
     ocr = OCR()
     img = cv2.imread("lib/ocr/test_images/PXL_20201117_200200158.jpg")
     text = ocr.get_text(img)
-    print(text)
 
 
 if __name__ == "__main__":
